79a107764ec8b5f618ac0b7a66ae64ff871e5a90
Bart Huiskamp <bartolameus.h@gmail.com>
2022-02-17 18:17:47 +0100

    B

--- models/pizza.model.js
+++ models/pizza.model.js
@@ -3,34 +3,10 @@ const { Schema, model } = require("mongoose");
 
 const pizzaSchema = new Schema(
   {
-      name: {
-        type: String,
-        required: [true, 'name of a pizza is required.'],
-        unique: true,
-      },
-      tags: [String],
-      toppings: [{type: Schema.Types.ObjectId, ref: "Ingredients"}],
-        // type: [String],
-        // enum: ["salami","mozarella","peperoni","ham","beef","paprika","tomato","olives",{type: Schema.Types.ObjectId, ref: "Author"},],
-      sauces: {
-        type: [String],
-        enum: ["tomato sauce", "crême fresh", "barbecue sauce" ]
-      },
-      baseCheese: {
-        type: [String],
-        enum:["shredded cheese","gouda","cheddar","gorgonzola","mozarella","bufflo mozarella","pecorino"],
-      },
-      seasoning:[{type: Schema.Types.ObjectId, ref: "Ingredients"}],
-      dough:{
-        type: String,
-        required: true,
-        enum: ["neapolitian", "gluten free","vegie dough"],
-        default: "neapolitian",
-        
-      },
-      imagesUrl: String,
-      details: String,
-      
+    name: {
+      type: String,
+      required: [true, 'name of a pizza is required.'],
+      unique: true,
     },
     tags: [String],
     toppings: [{ type: Schema.Types.ObjectId, ref: "Ingredients" }],
